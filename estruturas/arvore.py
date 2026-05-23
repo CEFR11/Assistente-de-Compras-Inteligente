d13883dc12b6086b5de0b786221e72dcd31ff0e1
josebarbosa-888 <jose.barbosa@penedo.ufal.br>
2026-05-22 21:41:03 -0300

    feat(arvore): expande vocabulario com categorias de Eletrodomesticos, Moda, Esportes e Games

--- estruturas/arvore.py
+++ estruturas/arvore.py
@@ -7,6 +7,35 @@ class NoArvore:
 class ArvoreCategorias:
     def __init__(self):
         self.raiz = NoArvore('raiz')
+        self._popular_categorias()
+
+    def _popular_categorias(self):
+        categorias = [
+            ['Eletrônicos', 'Celulares'],
+            ['Eletrônicos', 'Notebooks'],
+            ['Eletrônicos', 'Televisores'],
+            ['Eletrônicos', 'Fones'],
+            ['Eletrodomésticos', 'Geladeiras'],
+            ['Eletrodomésticos', 'Micro-ondas'],
+            ['Eletrodomésticos', 'Máquinas de Lavar'],
+            ['Moda', 'Masculino'],
+            ['Moda', 'Feminino'],
+            ['Moda', 'Calçados'],
+            ['Esportes', 'Musculação'],
+            ['Esportes', 'Corrida'],
+            ['Esportes', 'Futebol'],
+            ['Games', 'Consoles'],
+            ['Games', 'Acessórios'],
+        ]
+        for caminho in categorias:
+            self._criar_caminho(caminho)
+
+    def _criar_caminho(self, caminho):
+        no = self.raiz
+        for categoria in caminho:
+            if categoria not in no.filhos:
+                no.filhos[categoria] = NoArvore(categoria)
+            no = no.filhos[categoria]
 
     def inserir(self, caminho, produto):
         no = self.raiz
@@ -23,6 +52,17 @@ class ArvoreCategorias:
                 return []
             no = no.filhos[categoria]
         return self._coletar(no)
+
+    def listar_categorias(self):
+        resultado = []
+        self._percorrer(self.raiz, [], resultado)
+        return resultado
+
+    def _percorrer(self, no, caminho_atual, resultado):
+        if caminho_atual:
+            resultado.append(' > '.join(caminho_atual))
+        for nome, filho in no.filhos.items():
+            self._percorrer(filho, caminho_atual + [nome], resultado)
 
     def _coletar(self, no):
         resultado = list(no.produtos)
