332dc15d98d1e1dfe0a98b040d176de079d2e8ee
josebarbosa-888 <jose.barbosa@penedo.ufal.br>
2026-06-09 19:11:57 -0300

    Melhora NLP, adiciona atributos aos produtos e remove bibliotecas externas

--- estruturas/fila_prioridade.py
+++ estruturas/fila_prioridade.py
@@ -1,23 +1,64 @@
-import heapq
-
 class FilaPrioridade:
-
     def __init__(self):
-        self._heap = []
+        self._heap    = []
         self._counter = 0
 
+    @staticmethod
+    def _pai(i):
+        return (i - 1) // 2
+
+    @staticmethod
+    def _esq(i):
+        return 2 * i + 1
+
+    @staticmethod
+    def _dir(i):
+        return 2 * i + 2
+
+    def _subir(self, i):
+        while i > 0:
+            pai = self._pai(i)
+            if self._heap[i][0] > self._heap[pai][0]:
+                self._heap[i], self._heap[pai] = self._heap[pai], self._heap[i]
+                i = pai
+            else:
+                break
+
+    def _descer(self, i):
+        n = len(self._heap)
+        while True:
+            maior = i
+            esq   = self._esq(i)
+            dir_  = self._dir(i)
+            if esq < n and self._heap[esq][0] > self._heap[maior][0]:
+                maior = esq
+            if dir_ < n and self._heap[dir_][0] > self._heap[maior][0]:
+                maior = dir_
+            if maior == i:
+                break
+            self._heap[i], self._heap[maior] = self._heap[maior], self._heap[i]
+            i = maior
+
     def inserir(self, item: dict, prioridade: int):
-        heapq.heappush(self._heap, (-prioridade, self._counter, item))
+        self._heap.append((prioridade, self._counter, item))
         self._counter += 1
+        self._subir(len(self._heap) - 1)
 
     def extrair_max(self) -> dict:
         if self.esta_vazia():
             raise IndexError('Fila de prioridade vazia')
-        _, _, item = heapq.heappop(self._heap)
-        return item
+        if len(self._heap) == 1:
+            return self._heap.pop()[2]
+        topo = self._heap[0][2]
+        self._heap[0] = self._heap.pop()
+        self._descer(0)
+        return topo
 
     def esta_vazia(self) -> bool:
         return len(self._heap) == 0
+
+    def tamanho(self) -> int:
+        return len(self._heap)
 
     def calcular_prioridade(self, dados: dict) -> int:
         score = 0
@@ -26,3 +67,6 @@ class FilaPrioridade:
         if dados.get('marca'):     score += 2
         if dados.get('atributos'): score += len(dados['atributos'])
         return score
+
+    def __repr__(self):
+        return f'FilaPrioridade(tamanho={len(self._heap)})'
--- estruturas/pilha.py
+++ estruturas/pilha.py
@@ -1,40 +1,76 @@
-from collections import deque
+class _No:
+    def __init__(self, valor):
+        self.valor    = valor
+        self.anterior = None
+        self.proximo  = None
+
 
 class Pilha:
-
     def __init__(self, capacidade: int = 50):
-        self._dados = deque(maxlen=capacidade)
+        self._topo       = None
+        self._base       = None
+        self._tamanho    = 0
+        self._capacidade = capacidade
 
     def push(self, item):
-        self._dados.append(item)
+        no = _No(item)
+        if self._topo is None:
+            self._topo = self._base = no
+        else:
+            no.anterior        = self._topo
+            self._topo.proximo = no
+            self._topo         = no
+        self._tamanho += 1
+        if self._tamanho > self._capacidade:
+            self._remover_base()
+
+    def _remover_base(self):
+        if self._base is None:
+            return
+        self._base = self._base.proximo
+        if self._base is not None:
+            self._base.anterior = None
+        else:
+            self._topo = None
+        self._tamanho -= 1
 
     def pop(self):
         if self.esta_vazia():
             raise IndexError('Pilha vazia')
-        return self._dados.pop()
+        valor      = self._topo.valor
+        self._topo = self._topo.anterior
+        if self._topo is not None:
+            self._topo.proximo = None
+        else:
+            self._base = None
+        self._tamanho -= 1
+        return valor
 
     def voltar(self):
-        if len(self._dados) < 2:
+        if self._tamanho < 2:
             return None
-        self._dados.pop()
-        return self._dados[-1]
+        self.pop()
+        return self._topo.valor if self._topo else None
 
     def peek(self):
-        if self.esta_vazia():
-            return None
-        return self._dados[-1]
+        return None if self.esta_vazia() else self._topo.valor
 
     def esta_vazia(self) -> bool:
-        return len(self._dados) == 0
+        return self._topo is None
 
     def tamanho(self) -> int:
-        return len(self._dados)
+        return self._tamanho
 
     def historico(self, n: int = 5) -> list:
-        return list(self._dados)[-n:]
+        items = []
+        atual = self._topo
+        while atual and len(items) < n:
+            items.append(atual.valor)
+            atual = atual.anterior
+        return items[::-1]
 
     def categorias_recentes(self, n: int = 5) -> list:
-        return [h.get('categoria') for h in list(self._dados)[-n:] if h.get('categoria')]
+        return [h.get('categoria') for h in self.historico(n) if h.get('categoria')]
 
     def __repr__(self):
-        return f'Pilha({list(self._dados)})'
+        return f'Pilha({self.historico()})'
